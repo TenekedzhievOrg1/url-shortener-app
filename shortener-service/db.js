1970b7f9061d8ac086594d00dc9d55bc8893c885
Lyuben Tenekedzhiev <lyubentenekedzhiev@Lyubens-MacBook-Pro.local>
2025-10-04 23:41:41 +0300

    access db credentials programatically

--- shortener-service/db.js
+++ shortener-service/db.js
@@ -1,10 +1,21 @@
 import { Pool } from "pg";
 import dotenv from "dotenv";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const client = new SecretsManagerClient({ region: process.env.AWS_REGION });
+const command = new GetSecretValueCommand({
+  SecretId: process.env.AWS_SECRETS_MANAGER_NAME,
+});
+const secret = await client.send(command);
+const { username, password } = JSON.parse(secret.SecretString);
 
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
+  connectionString: `postgres://${username}:${password}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
 });
 
 export default pool;
