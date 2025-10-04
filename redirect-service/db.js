6be9e0d9c99c4f76217bd21ef319ff74a462fa3b
Lyuben Tenekedzhiev <lyubentenekedzhiev@Lyubens-MacBook-Pro.local>
2025-10-04 22:47:05 +0300

    change connectionString construction

--- redirect-service/db.js
+++ redirect-service/db.js
@@ -4,8 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_URL,
+  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
 });
 
 export default pool;
