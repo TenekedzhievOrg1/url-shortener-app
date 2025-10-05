2be173b36ed0c0a216e3c1747db610fd36d9d3f5
Lyuben Tenekedzhiev <lyubentenekedzhiev@Lyubens-MacBook-Pro.local>
2025-10-05 14:37:34 +0300

    update db connections

--- shortener-service/db.js
+++ shortener-service/db.js
@@ -5,17 +5,32 @@ import {
   GetSecretValueCommand,
 } from "@aws-sdk/client-secrets-manager";
 
-const client = new SecretsManagerClient({ region: process.env.AWS_REGION });
-const command = new GetSecretValueCommand({
-  SecretId: process.env.AWS_SECRETS_MANAGER_NAME,
-});
-const secret = await client.send(command);
-const { username, password } = JSON.parse(secret.SecretString);
-
 dotenv.config();
 
-const pool = new Pool({
-  connectionString: `postgres://${username}:${password}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
-});
+async function getDbCredentials() {
+  try {
+    const client = new SecretsManagerClient({ region: process.env.AWS_REGION });
+    const command = new GetSecretValueCommand({
+      SecretId: process.env.AWS_SECRETS_MANAGER_NAME,
+    });
+    const secret = await client.send(command);
+    return JSON.parse(secret.SecretString);
+  } catch (err) {
+    console.error("Failed to fetch DB credentials from Secrets Manager:", err);
+    throw err;
+  }
+}
+
+async function createPool() {
+  const { username, password } = await getDbCredentials();
+
+  const pool = new Pool({
+    connectionString: `postgres://${username}:${password}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
+  });
+
+  return pool;
+}
+
+const pool = await createPool();
 
 export default pool;
